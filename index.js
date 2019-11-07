1ba309bb3330164b6f68de9b940e5d0080ea6e52
sziklai10 <57219849+sziklai10@users.noreply.github.com>
2019-11-07 09:55:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express'); //setting express
 const Datastore = require('nedb');//setting Datastore
 
 const app = express(); //setting app
-app.listen(3000, () => console.log("listening at port 3000"));// setting up host (3000 if local server, port number if external server)
-app.use(express.static('public'));//using public file
-app.use(express.json({limit: '1mb'}));
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Starting server at ${port}`);
 });
+app.use(express.static('public'));//using public file
+app.use(express.json({limit: '1mb'}));
 
 const database = new Datastore('database.db');//new database
 database.loadDatabase();//loading the database
